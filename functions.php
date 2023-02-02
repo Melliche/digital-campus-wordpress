8ec788c336372e14ae46ea58f3e1491b49fcdd64
Julick Mellah <j.mellah@icloud.com>
2023-02-02 16:11:11 +0100

    header

--- functions.php
+++ functions.php
@@ -2,18 +2,32 @@
 
 
 // Ajouter la prise en charge des images mises en avant
-add_theme_support( 'post-thumbnails' );
+add_theme_support('post-thumbnails');
 
 
 // Ajouter automatiquement le titre du site dans l'en-tête du site
-add_theme_support( 'title-tag' );
+add_theme_support('title-tag');
 
 
 // Ajouter des classes au body
-function my_plugin_body_class($classes) {
+function my_plugin_body_class($classes)
+{
     $classes[] = 'flex h-full flex-col bg-zinc-50 dark:bg-black';
     return $classes;
 }
 
-
 add_filter('body_class', 'my_plugin_body_class');
+
+// Ajouter un emplacement de menu
+function theme_register_nav_menu()
+{
+    register_nav_menus(array(
+        'menu_principal' => __('Menu Principal', 'text_domain'),
+    ));
+}
+
+add_action('after_setup_theme', 'theme_register_nav_menu', 0);
+
+if (function_exists('acf_add_options_page')) {
+    acf_add_options_page();
+}
