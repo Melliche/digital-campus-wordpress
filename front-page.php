a6e18da32d91ae4baf533b1acee1eefa17384089
Yanissmrl <90864149+Yanissmrl@users.noreply.github.com>
2023-02-08 16:00:05 +0100

    crea section 7

--- front-page.php
+++ front-page.php
@@ -97,42 +97,75 @@
 
     </div>
     <div>
-        
-    <div class="grid grid-cols-2 w-full">
-        <div class="bg-orange px-11rem py-7 text-white">
-            <h2 class="text-9xl	font-medium mb-7"><?php the_field('section5_title'); ?></h2>
-            <p class="text-lg mb-7 w-70%"><?php the_field('section5_text'); ?></p>
-            <nav class="pointer-events-auto hidden md:block">
-                <ul class="grid grid-cols-2">
+
+        <div class="grid grid-cols-2 w-full">
+            <div class="bg-orange px-12rem py-7 text-white">
+                <h2 class="text-9xl	font-medium mb-7"><?php the_field('section5_title'); ?></h2>
+                <p class="text-lg mb-7 w-70%"><?php the_field('section5_text'); ?></p>
+                <nav class="pointer-events-auto hidden md:block">
+                    <ul class="grid grid-cols-2">
+                        <?php
+                        // On récupère la liste des menus
+                        $menuLocations = get_nav_menu_locations();
+                        // On récupère l'ID de notre menu principal
+                        $menuID = $menuLocations['liste_campus'];
+                        // On récupère les liens de ce menu
+                        $menu = wp_get_nav_menu_items($menuID);
+                        // On boucle dans les liens et on les affiche
+                        foreach ($menu as $navItem) {
+                            echo '<li class="py-2"><a class="text-2xl font-medium text-gray-400" href="' . $navItem->url . '" title="' . $navItem->title . '">' . $navItem->title . '</a></li>';
+                        }
+                        ?>
+                    </ul>
+                </nav>
+            </div>
+
+            <div>
+                <div class="h-full w-full">
                     <?php
-                    // On récupère la liste des menus
-                    $menuLocations = get_nav_menu_locations();
-                    // On récupère l'ID de notre menu principal
-                    $menuID = $menuLocations['liste_campus'];
-                    // On récupère les liens de ce menu
-                    $menu = wp_get_nav_menu_items($menuID);
-                    // On boucle dans les liens et on les affiche
-                    foreach ($menu as $navItem) {
-                        echo '<li class="py-2"><a class="text-2xl font-medium text-gray-400" href="' . $navItem->url . '" title="' . $navItem->title . '">' . $navItem->title . '</a></li>';
+                    $image = get_field('section5_image');
+                    $size = 'presentation-size-frontpage';
+                    if ($image) {
+                        echo wp_get_attachment_image($image, $size);
                     }
                     ?>
-                </ul>
-            </nav>
-        </div>
-        
-        <div>
-            <div class="h-full w-full">
-                <?php
-                $image = get_field('section5_image');
-                $size = 'presentation-size-frontpage';
-                if ($image) {
-                    echo wp_get_attachment_image($image, $size);
-                }
-                ?>
+                </div>
             </div>
         </div>
-    </div>
 
+        <div class="bg-gray py-20">
+            <div class="mycontainer">
+                <div class="flex flex-col items-center">
+                    <h2 class="text-7xl	"><?php the_field('section6_title'); ?></h2>
+                    <p class="py-5 w-23% text-center	"><?php the_field('section6_text'); ?></p>
+                    <div class="grid grid-cols-5 gap-4">
+                        <?php
+
+                        // Check rows existexists.
+                        if (have_rows('section6_images')) :
+
+                            // Loop through rows.
+                            while (have_rows('section6_images')) : the_row();
+
+                                // Load sub field value.
+                                $image = get_sub_field('image');
+                                $size = 'card-size-frontpage';
+                                if ($image) {
+                                    echo wp_get_attachment_image($image, $size);
+                                }
+
+
+                            // End loop.
+                            endwhile;
+
+                        // No value.
+                        else :
+                        // Do something...
+                        endif; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
 </section>
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,25 +7,27 @@ module.exports = {
         'max': '1520px',
       },
       spacing: {
-        '11rem': '11.5rem',
+        '12rem': '12rem',
         '26rem': '26rem',
         '36rem': '36rem',
         '60rem': '60rem',
+        '23%': '23%',
         '30%': '30%',
         '70%': '70%',
-      }
+      },
+      fontFamily: {
+        display: ["Baloo 2", "cursive"],
+      },
+      colors: {
+        'orange': '#F87C56',
+        'blue': '#0092BD',
+        'gray': '#EBEBEB',
+        'gray-grass': '#a1a1a1',
+        'white': '#FFFFFF',
+        'black': '#000000',
+      },
     },
-    fontFamily: {
-      display: ["Baloo 2", "cursive"],
-    },
-    colors: {
-      'orange': '#F87C56',
-      'blue': '#0092BD',
-      'gray': '#EBEBEB',
-      'gray-grass': '#a1a1a1',
-      'white': '#FFFFFF',
-      'black': '#000000',
-    },
+
   },
   plugins: [],
 }
