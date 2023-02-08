9455d05ea0acc2bb89af262f46978757d529e06c
Julick Mellah <74454432+Melliche@users.noreply.github.com>
2023-02-08 15:18:36 +0100

    correctifs mr header

--- header.php
+++ header.php
@@ -3,10 +3,11 @@
 
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;700;800&display=swap"
+          rel="stylesheet">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
     <?php wp_head(); ?>
 </head>
@@ -29,30 +30,19 @@
     <nav>
         <ul class="hidden flex-row sm:flex">
             <?php
-            $image = get_field('logo', 'options');
-            $size = 'footer-size';
-            if ($image) {
-                echo wp_get_attachment_image($image, $size);
+            // On récupère la liste des menus
+            $menuLocations = get_nav_menu_locations();
+            // On récupère l'ID de notre menu principal
+            $menuID = $menuLocations['menu_principal'];
+            // On récupère les liens de ce menu
+            $menu = wp_get_nav_menu_items($menuID);
+            // On boucle dans les liens et on les affiche
+            foreach ($menu as $navItem) {
+                echo '<li><a class="font-semibold px-2" href="' . $navItem->url . '" title="' . $navItem->title . '">' . $navItem->title . '</a></li>';
             }
             ?>
-        </div>
-        <div class="flex text-xl font-semibold text-gray-500 px-2 sm:hidden">Menu</div>
-        <nav>
-            <ul class="hidden flex-row sm:flex">
-                <?php
-                // On récupère la liste des menus
-                $menuLocations = get_nav_menu_locations();
-                // On récupère l'ID de notre menu principal
-                $menuID = $menuLocations['menu_principal'];
-                // On récupère les liens de ce menu
-                $menu = wp_get_nav_menu_items($menuID);
-                // On boucle dans les liens et on les affiche
-                foreach ($menu as $navItem) {
-                    echo '<li><a class="font-semibold px-2" href="' . $navItem->url . '" title="' . $navItem->title . '">' . $navItem->title . '</a></li>';
-                }
-                ?>
-            </ul>
-        </nav>
-    </header>
+        </ul>
+    </nav>
+</header>
 
-    <?php wp_body_open(); ?>
+<?php wp_body_open(); ?>
