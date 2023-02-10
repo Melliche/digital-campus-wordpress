ced053aca486966d5a6a6c91cba69faf0aebf749
Julick Mellah <74454432+Melliche@users.noreply.github.com>
2023-02-10 09:57:38 +0100

    page contact responsive et refacto css

---
+++ template-contact.php
@@ -0,0 +1,33 @@
+<?php /* Template Name: Page Contact */ ?>
+
+<?php get_header(); ?>
+
+
+<section class="bg-black min-h-screen">
+    <div class="grid grid-cols-6">
+        <a class="grid col-start-1 col-span-2 my-12" href="/">
+            <div class="flex items-center justify-center h-8 px-8">
+                <?php
+                $image = get_field('logo');
+                $size = 'footer-size';
+                if ($image) {
+                    echo wp_get_attachment_image($image, $size);
+                }
+                ?>
+            </div>
+        </a>
+        <div class="col-start-3 col-span-3 place-items-center">
+            <h1 class="col-start-4 text-white text-center text-xl md:text-5xl my-8"><?= get_field('titre'); ?></h1>
+            <div class="place-items-center text-center">
+                <?php echo FrmFormsController::get_form_shortcode(array('id' => 2)); ?>
+            </div>
+            <div class="text-[0.7rem] text-center my-4 place-items-center"><?= get_field('texte'); ?></div>
+
+        </div>
+    </div>
+
+
+
+</section>
+
+<?php get_footer(); ?>
